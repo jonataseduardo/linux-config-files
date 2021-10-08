6d7ed5de33eda8b0e4f0caba75cff67057043849
jonataseduardo <jonataseduardo@gmail.com>
2021-10-08 11:39:19 -0300

    adding spell map, corrected

--- neovim/lua/keymaps.lua
+++ neovim/lua/keymaps.lua
@@ -2,9 +2,9 @@ local map = vim.api.nvim_set_keymap
 local cmd = vim.cmd
 
 -- Save with w!! files with sudo permition
-map('c', 'w!!', 'execute 'silent! write sudo tee % >/dev/null' <bar> edit!', {noremap = true})
+map('c', 'w!!', "execute 'silent! write sudo tee % >/dev/null' <bar> edit!", {noremap = true})
 
 
 -- Spell
-map('n', '<leader>pt :setlocal spell! spelllang=pt<cr>', {noremap = true})
-map('n', '<leader>en :setlocal spell! spelllang=en<cr>', {noremap = true})
+map('n', '<leader>pt',  ':setlocal spell! spelllang=pt<cr>', {noremap = true})
+map('n', '<leader>en', ':setlocal spell! spelllang=en<cr>', {noremap = true})
